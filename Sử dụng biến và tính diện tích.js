3b6b1fa4f5654c248efb8c7b48b531781dc2a4bb
datgiaosu2k1 <datgiaosu2k1@gmail.com>
2025-07-24 02:32:53 +0900

    upload BTVN session 10-03

---
+++ Sử dụng biến và tính diện tích.js
@@ -0,0 +1,4 @@
+let width = prompt("Nhập chiều rộng hình chữ nhật:");
+let height = prompt("Nhập chiều cao hình chữ nhật:");
+let area = width * height;
+document.write("Diện tích hình chữ nhật là: " + area);
