a43c5c4777101dea9c77a4f458f335c32170ec98
datgiaosu2k1 <datgiaosu2k1@gmail.com>
2025-07-24 02:54:33 +0900

    upload BTVN session 10-05

---
+++ Bài toán chuyển đổi tiền tệ.js
@@ -0,0 +1,5 @@
+let dola = +prompt("Nhập số tiền đô la cần chuyển đổi: ");
+let VND = dola * 25000;
+alert("với: " + dola +"$" + "=" + VND+ "VNĐ");
+console.log("với: " + dola +"$" + "=" + VND+ "VNĐ");
+document.write("với: " + dola +"$" + "=" + VND+ "VNĐ");
