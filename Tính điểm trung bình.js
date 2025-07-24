71d661db98f1f21f63c2dab310b9dc4b9e98eaa8
datgiaosu2k1 <datgiaosu2k1@gmail.com>
2025-07-24 13:20:48 +0900

    upload BTVN session 10-06

---
+++ Tính điểm trung bình.js
@@ -0,0 +1,6 @@
+let math = +prompt("nhập điểm toán");
+let physics = +prompt("nhập điểm bật lý");
+let chemistry = +prompt ("nhập điểm hoá");
+let score = (math + physics + chemistry)/3;
+document.write("My average score in Math, Physics, and Chemistry is: "+score);
+alert("My average score in Math, Physics, and Chemistry is: " + score)
